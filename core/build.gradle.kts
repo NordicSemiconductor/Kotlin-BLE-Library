dcc2af7d539533112dd5b68448443152b1c8b756
Sylwester Zielinski <sylwester.zielinski@nordicsemi.no>
2023-02-20 11:49:34 +0100

    Add releasing configuration

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -29,14 +29,29 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
- plugins {
+plugins {
     alias(libs.plugins.nordic.feature)
     alias(libs.plugins.nordic.hilt)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.nordic.nexus)
 }
 
 group = "no.nordicsemi.android.kotlin.ble"
 
+nordicNexusPublishing {
+    POM_ARTIFACT_ID = "core"
+    POM_NAME = "Nordic Kotlin library for BLE connection"
+
+    POM_DESCRIPTION = "Nordic Android Kotlin BLE library"
+    POM_URL = "https://github.com/NordicPlayground/Android-Common-Libraries"
+    POM_SCM_URL = "https://github.com/NordicPlayground/Android-Common-Libraries"
+    POM_SCM_CONNECTION = "scm:git@github.com:NordicPlayground/Android-Common-Libraries.git"
+    POM_SCM_DEV_CONNECTION = "scm:git@github.com:NordicPlayground/Android-Common-Libraries.git"
+
+    POM_DEVELOPER_ID = "syzi"
+    POM_DEVELOPER_NAME = "Sylwester Zieliński"
+    POM_DEVELOPER_EMAIL = "sylwester.zielinski@nordicsemi.no"
+}
 
 android {
     namespace = "no.nordicsemi.android.kotlin.ble.core"
