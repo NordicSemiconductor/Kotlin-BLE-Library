80ee3413d61f3f1455e288bedcd06b9330351402
Sylwester Zielinski <sylwester.zielinski@nordicsemi.no>
2023-04-04 14:25:56 +0200

    Add maven publishing for private modules

--- server-mock/build.gradle.kts
+++ server-mock/build.gradle.kts
@@ -2,9 +2,25 @@ plugins {
     alias(libs.plugins.nordic.feature)
     alias(libs.plugins.nordic.hilt)
     alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.nordic.nexus)
 }
 
 group = "no.nordicsemi.android.kotlin.ble"
+
+nordicNexusPublishing {
+    POM_ARTIFACT_ID = "server-mock"
+    POM_NAME = "Nordic Kotlin library for BLE server side."
+
+    POM_DESCRIPTION = "Nordic Android Kotlin BLE library"
+    POM_URL = "https://github.com/NordicPlayground/Kotlin-BLE-Library"
+    POM_SCM_URL = "https://github.com/NordicPlayground/Kotlin-BLE-Library"
+    POM_SCM_CONNECTION = "scm:git@github.com:NordicPlayground/Kotlin-BLE-Library.git"
+    POM_SCM_DEV_CONNECTION = "scm:git@github.com:NordicPlayground/Kotlin-BLE-Library.git"
+
+    POM_DEVELOPER_ID = "syzi"
+    POM_DEVELOPER_NAME = "Sylwester Zieliński"
+    POM_DEVELOPER_EMAIL = "sylwester.zielinski@nordicsemi.no"
+}
 
 android {
     namespace = "no.nordicsemi.android.kotlin.ble.server.mock"
