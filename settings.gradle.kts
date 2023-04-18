50de20e8e26bbc369abadb6c26f75f37a627d144
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2023-04-18 23:44:12 +0200

    Bumps to gradle 8.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -48,7 +48,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nordicsemi.android.gradle:version-catalog:1.3.3")
+            from("no.nordicsemi.android.gradle:version-catalog:1.4.2")
         }
     }
 }
