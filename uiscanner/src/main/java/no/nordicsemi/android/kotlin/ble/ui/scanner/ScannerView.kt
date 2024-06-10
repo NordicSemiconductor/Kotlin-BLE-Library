aea376cc9681f5e21bef3aa2cd121ff4af4f5c21
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-06-10 14:33:04 +0200

    Optimizes imports

--- uiscanner/src/main/java/no/nordicsemi/android/kotlin/ble/ui/scanner/ScannerView.kt
+++ uiscanner/src/main/java/no/nordicsemi/android/kotlin/ble/ui/scanner/ScannerView.kt
@@ -33,15 +33,17 @@ package no.nordicsemi.android.kotlin.ble.ui.scanner
 
 import android.os.ParcelUuid
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
