35e16cd9ec772ecb16bbacaa9fb2d46042866562
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2025-12-18 15:17:32 +0100

    Migration to Kotlin 2.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -68,7 +68,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         // Use Nordic Gradle Version Catalog with common external libraries versions.
         create("libs") {
-            from("no.nordicsemi.android.gradle:version-catalog:2.11")
+            from("no.nordicsemi.android.gradle:version-catalog:2.11.1")
         }
         // Fixed versions for Nordic libraries.
         create("nordic") {
