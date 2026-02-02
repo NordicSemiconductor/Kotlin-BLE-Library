fd87f1e5832ad0e30aed6387a33058516953b06f
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2026-02-02 11:52:40 +0100

    Common environment-android-compose module added

---
+++ environment-android-compose/Module.md
@@ -0,0 +1,10 @@
+# Module environment-android-mock-compose
+
+Set of Jetpack Compose utilities for Mock Environment on Android.
+
+# Package no.nordicsemi.kotlin.ble.android.compose
+
+This package contains helper Composables to apply mock environment in tests.
+
+Use `LocalEnvironmentProvider` to redirect permission requests from `rememberLauncherForActivityResult`
+to the mock environment.
---
+++ environment-android-compose/build.gradle.kts
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2026, Nordic Semiconductor
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ * of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without specific prior
+ * written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+plugins {
+    alias(libs.plugins.nordic.library.compose)
+    alias(libs.plugins.nordic.nexus.android)
+}
+
+group = "no.nordicsemi.kotlin.ble"
+
+nordicNexusPublishing {
+    POM_ARTIFACT_ID = "core-compose"
+    POM_NAME = "Utils for Compose"
+    POM_DESCRIPTION = "Set of utilities for Jetpack Compose."
+    POM_URL = "https://github.com/NordicSemiconductor/Kotlin-BLE-Library"
+    POM_SCM_URL = "https://github.com/NordicSemiconductor/Kotlin-BLE-Library"
+    POM_SCM_CONNECTION = "scm:git@github.com:NordicSemiconductor/Kotlin-BLE-Library.git"
+    POM_SCM_DEV_CONNECTION = "scm:git@github.com:NordicSemiconductor/Kotlin-BLE-Library.git"
+}
+
+android {
+    namespace = "no.nordicsemi.kotlin.ble.core.compose"
+}
+
+dependencies {
+    api(project(":core-android"))
+    // These 2 dependencies are optional. At least one of them has to
+    // be added to the final project with "implementation".
+    compileOnly(project(":environment-android"))
+    compileOnly(project(":environment-android-mock"))
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.runtime)
+}
+
+dokka {
+    dokkaSourceSets.named("main") {
+        includes.from("Module.md")
+    }
+}
