77ecc3388326f5345ff778ed3557dd1a6a19c2d2
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2026-02-11 14:52:11 +0100

    Nordic Gradle Plugin updated to 2.11.3-1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -68,7 +68,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         // Use Nordic Gradle Version Catalog with common external libraries versions.
         create("libs") {
-            from("no.nordicsemi.android.gradle:version-catalog:2.11.3")
+            from("no.nordicsemi.android.gradle:version-catalog:2.11.3-1")
         }
         // Fixed versions for Nordic libraries.
         create("nordic") {
